f878686894c6c7fda6830a3d6b0db9a3be3ade20
Okens <Ng.Eric314@gmail.com>
2022-04-09 20:15:54 -0700

    Adding uuid package, adding post route, adding addNote function to notes lib.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const router = require('express').Router();
-const { getNotes } = require('../../lib/notes');
+const { getNotes, addNote } = require('../../lib/notes');
 const fs = require('fs');
+const {v4: uuid } = require('uuid');
+
 
 router.get('/notes', function(req, res) {
   console.log(`${req.method} route /api/notes`)
@@ -10,10 +12,15 @@ router.get('/notes', function(req, res) {
 });
 
 router.post('/notes', function(req, res) {
-  const notes = getNotes();
+  const {title, text} = req.body
+  const id = uuid()
+  const newNote = {id, title, text}
+  addNote(newNote)
 
-
-
+  const response = {
+    status: 'success'
+  }
+  res.json(response);
   console.log(`${req.method} route /api/notes`)
 });
 

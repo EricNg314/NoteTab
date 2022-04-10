21821bd882f4eb73c8f6ea86641b1dffaad397f4
Okens <Ng.Eric314@gmail.com>
2022-04-09 22:53:57 -0700

    Adding delete function and route.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
-const { getNotes, addNote } = require('../../lib/notes');
+const { getNotes, addNote, deleteNote} = require('../../lib/notes');
 const fs = require('fs');
 const {v4: uuid } = require('uuid');
 
@@ -24,8 +24,16 @@ router.post('/notes', function(req, res) {
   console.log(`${req.method} route /api/notes`)
 });
 
-router.post('/notes/delete:id', function(req, res) {
-  console.log(`${req.method} route /api/notes`)
+router.delete('/notes/:id', function(req, res) {
+
+  const {id} = req.params
+  deleteNote(id)
+
+  const response = {
+    status: 'success'
+  }
+  res.json(response);
+  console.log(`${req.method} route /notes/:id`)
 });
 
 

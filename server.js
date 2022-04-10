8989e7e487953b7fd998455e3e934526a77a93b8
Okens <Ng.Eric314@gmail.com>
2022-04-09 18:00:44 -0700

    Updating server.js to remove catch all route.

--- server.js
+++ server.js
@@ -8,27 +8,34 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
+  console.log(`${req.method} route /`)
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', function(req, res) {
+  console.log(`${req.method} route /notes`)
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('/api/notes', function(req, res) {
+  console.log(`${req.method} route /api/notes`)
+  const rawData = fs.readFileSync('./db/db.json');
+  const notes = JSON.parse(rawData);
+  res.json(notes);
 });
 
 app.post('/api/notes', function(req, res) {
+  console.log(`${req.method} route /api/notes`)
 });
 
 app.post('/api/notes/delete', function(req, res) {
+  console.log(`${req.method} route /api/notes`)
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+
 
 app.listen(PORT, () => 
 console.log(`App listening on http://localhost:${PORT}`))

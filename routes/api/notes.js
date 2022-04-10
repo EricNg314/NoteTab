234b2eb8b64f5edee02493de16bfdd33ee6067be
Okens <Ng.Eric314@gmail.com>
2022-04-09 19:38:14 -0700

    Adding library for notes functions.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const router = require('express').Router();
+const { getNotes } = require('../../lib/notes');
 const fs = require('fs');
 
 router.get('/notes', function(req, res) {
   console.log(`${req.method} route /api/notes`)
-  const rawData = fs.readFileSync('./db/db.json');
-  const notes = JSON.parse(rawData);
+  const notes = getNotes();
   res.json(notes);
 });
 
 router.post('/notes', function(req, res) {
+  const notes = getNotes();
+
+
+
   console.log(`${req.method} route /api/notes`)
 });
 
